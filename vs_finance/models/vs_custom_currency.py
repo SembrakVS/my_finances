a699dfc6cf964e8bccfa398a1c6045b6900e0f50
Vasyl Sembrak <vsembrak@gmail.com>
2024-04-22 00:50:33 +0000

    [ADD] docstring

--- vs_finance/models/vs_custom_currency.py
+++ vs_finance/models/vs_custom_currency.py
@@ -2,6 +2,19 @@ from odoo import fields, models
 
 
 class CustomCurrency(models.Model):
+    """
+    Custom Currency Model.
+
+    This model extends the standard Odoo res.currency model
+    by adding a Many2many relationship with vs.bill to store
+    information about bills associated with each currency.
+
+    Attributes:
+        _inherit (str): The name of the model to inherit from.
+        _name (str): The technical name of the model.
+        bill_ids (Many2many): Relationship field to store the associated bills.
+    """
+
     _inherit = 'res.currency'
 
     bill_ids = fields.Many2many('vs.bill', string='Bills')
